2c3b18e4efe25d0ea5a473d0f299408d92dbff7a
DianaSuf <129599628+DianaSuf@users.noreply.github.com>
2023-12-22 01:43:25 +0500

    Перламутровые пуговицы

--- js/main.js
+++ js/main.js
@@ -2,12 +2,15 @@ import {renderGallery} from './gallery.js';
 import {initEditPopup} from './form.js';
 import {getData} from './api.js';
 import {showAlert} from './util.js';
+import {initFilters} from './sort.js';
+import { debounce } from './util.js';
 
 initEditPopup();
 
 getData()
   .then((data) => {
     renderGallery(data);
+    initFilters(data, debounce(renderGallery));
   })
   .catch(
     (err) => {
--- js/util.js
+++ js/util.js
@@ -21,6 +21,69 @@ const showAlert = (message) => {
   }, ALERT_SHOW_TIME);
 };
 
+function debounce (callback, timeoutDelay = 500) {
+  // Используем замыкания, чтобы id таймаута у нас навсегда приклеился
+  // к возвращаемой функции с setTimeout, тогда мы его сможем перезаписывать
+  let timeoutId;
+
+  return (...rest) => {
+    // Перед каждым новым вызовом удаляем предыдущий таймаут,
+    // чтобы они не накапливались
+    clearTimeout(timeoutId);
+
+    // Затем устанавливаем новый таймаут с вызовом колбэка на ту же задержку
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+
+    // Таким образом цикл «поставить таймаут - удалить таймаут» будет выполняться,
+    // пока действие совершается чаще, чем переданная задержка timeoutDelay
+  };
+}
+
+function throttle (callback, delayBetweenFrames) {
+  // Используем замыкания, чтобы время "последнего кадра" навсегда приклеилось
+  // к возвращаемой функции с условием, тогда мы его сможем перезаписывать
+  let lastTime = 0;
+
+  return (...rest) => {
+    // Получаем текущую дату в миллисекундах,
+    // чтобы можно было в дальнейшем
+    // вычислять разницу между кадрами
+    const now = new Date();
+
+    // Если время между кадрами больше задержки,
+    // вызываем наш колбэк и перезаписываем lastTime
+    // временем "последнего кадра"
+    if (now - lastTime >= delayBetweenFrames) {
+      callback.apply(this, rest);
+      lastTime = now;
+    }
+  };
+}
+
+function getRandomInteger (min, max) {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+}
+
+const getRandomElements = (elements, randomElementsCount) => {
+  const elementNumbers = [];
+  const randomElements = [];
+  for (let i = 0; i < elements.length; i++) {
+    const number = getRandomInteger(0, elements.length - 1);
+    if (elementNumbers.indexOf(number) === -1) {
+      randomElements.push(elements[number]);
+      elementNumbers.push(number);
+    }
+    if (randomElements.length === randomElementsCount) {
+      break;
+    }
+  }
+  return randomElements;
+};
+
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
-export {showAlert, isEscapeKey};
+export {showAlert, debounce, throttle, isEscapeKey, getRandomElements};
